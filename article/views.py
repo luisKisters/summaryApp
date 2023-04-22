e2dfc7c9b58b5644a10f1ba91fe7af67f03f2c5f
luisKisters <luis.w.kisters@gmail.com>
2023-04-22 16:25:02 +0200

    Changed the view so that it outputs the article with data as json

--- article/views.py
+++ article/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .article_api import get_articles
+import json
 
 # Create your views here.
 
-def index(request):
-    data = get_articles("bitcoin", period=7)
-    context = {"data": data}
-    return render(request, "index.html", context)
+def article_view(request):
+    data = get_articles("politics", period=15)
+    json_data = json.dumps(data)
+    return HttpResponse(json_data, content_type="application/json")

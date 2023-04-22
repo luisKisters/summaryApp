bb9b814c9f88f119f7055b8abbe3de42abc90a28
luisKisters <luis.w.kisters@gmail.com>
2023-04-22 16:30:05 +0200

    Added that program gets summary from article api and then correctly summarizes it

--- summarize/views.py
+++ summarize/views.py
@@ -1,20 +1,46 @@
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
+import json
 import openai
+import requests
+import os
 
-# Create your views here.
-
-with open('api_key.txt', 'r') as file:
-    api_key = file.read()
+# get the current directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# append the path to api_keys.txt
+api_key_file = os.path.join(BASE_DIR, 'api_key.txt')
+# open the file
+with open(api_key_file, 'r') as file:
+    api_key = file.read().strip()
 
 openai.api_key = api_key
 
-res = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "summarize the input, only output the summary, not more, the output should sound like a news article", 
-        "content": ""},
+def summarize_article():
+    # Get JSON data from localhost:8000/api/article
+    response = requests.get('http://localhost:8000/api/article')
+    article_data = json.loads(response.text)
 
-    ]
-)
+    # Get the article content and summarize it using OpenAI's GPT-3 API
+    article_content = article_data['article_content']
+    content = f'summarize this article, make it so that it sounds as if it was a article on its own, 100 words long {article_content})'
+    content = str(content)
+    res = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "assistant", 
+            "content": content},
 
-print(res)
+        ]
+    )
+    
+    # Add the summary to the JSON data
+    article_data['summary'] = res
+
+    # Render the JSON data in the browser
+    return JsonResponse(article_data)
+    return response
+
+    
+def summary_view(request):
+    response = summarize_article()
+    return HttpResponse(response.content, content_type="application/json")

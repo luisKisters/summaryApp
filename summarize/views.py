74f43803a05e233cc29714b31d2f2671f0e66abb
luisKisters <luis.w.kisters@gmail.com>
2023-04-23 19:13:45 +0200

    Changed output type, added parameters

--- summarize/views.py
+++ summarize/views.py
@@ -15,24 +15,25 @@ with open(api_key_file, 'r') as file:
 
 openai.api_key = api_key
 
-def summarize_article():
+
+def summarize_article(words, language):
     # Get JSON data from localhost:8000/api/article
     response = requests.get('http://localhost:8000/api/article')
     article_data = json.loads(response.text)
 
     # Get the article content and summarize it using OpenAI's GPT-3 API
     article_content = article_data['article_content']
-    content = f'summarize this article, make it so that it sounds as if it was a article on its own, 100 words long {article_content})'
+    content = f'summarize this article, make it so that it sounds as if it was a article on its own, in language {language}, {words} words long, this is the article {article_content})'
     content = str(content)
     res = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "assistant", 
-            "content": content},
+            {"role": "assistant",
+             "content": content},
 
         ]
     )
-    
+
     # Add the summary to the JSON data
     article_data['summary'] = res
 
@@ -40,7 +41,7 @@ def summarize_article():
     return JsonResponse(article_data)
     return response
 
-    
-def summary_view(request):
-    response = summarize_article()
+
+def summary_view(request, lang='EN', words=100):
+    response = summarize_article(words=words, language=lang)
     return HttpResponse(response.content, content_type="application/json")

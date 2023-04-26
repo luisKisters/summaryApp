55a5b2fb707bab443b64c0447ec125a7501f4425
luisKisters <luis.w.kisters@gmail.com>
2023-04-26 20:37:07 +0200

    Changed unimportant stuff in settings.py

--- summaryApp/settings.py
+++ summaryApp/settings.py
@@ -129,6 +129,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8000",
+#     "http://localhost:3000",
+#     "http://127.0.0.1:8000",
+#     "http://127.0.0.1:3000",
+# ]
 
 CORS_ALLOW_METHODS = ['DELETE',    'GET',
                       'OPTIONS',    'PATCH',    'POST',    'PUT',]
@@ -137,6 +143,3 @@ CORS_ALLOW_HEADERS = ['accept',    'accept-encoding',    'authorization',    'co
                       'dnt',    'origin',    'user-agent',    'x-csrftoken',    'x-requested-with',]
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_METHODS = ['DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT']
-CORS_ALLOW_HEADERS = ['accept', 'accept-encoding', 'authorization',
-                      'content-type', 'dnt', 'origin', 'user-agent', 'x-csrftoken', 'x-requested-with']

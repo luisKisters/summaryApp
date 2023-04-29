42fec235f2ba58b515aa01fce506d6131468a63d
luisKisters <luis.w.kisters@gmail.com>
2023-04-29 17:30:35 +0200

    Changed view function so that it views the django frontend template

--- pages/views.py
+++ pages/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def index_view():
-    return render('')
+def index_view(request, *args, **kwargs):
+    return render(request, 'index.html')

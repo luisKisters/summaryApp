9c3f296ca66cce25bf9c86c0aabbbc5b4d7043b9
luisKisters <luis.w.kisters@gmail.com>
2023-05-01 19:10:50 +0200

    Viewing the fields of the API response works now

--- frontend/src/components/Summarize.jsx
+++ frontend/src/components/Summarize.jsx
@@ -11,6 +11,10 @@ export const Summary = () => {
         setWordCount(Number(event.target.value));
     }
 
+    const clearSummary = (event) => {
+        setSummary('')
+    }
+
     const handleFormSubmit = (event) => {
         event.preventDefault();
 
@@ -24,8 +28,8 @@ export const Summary = () => {
                     }
                 })
                     .then(response => {
-                        const summary = response;
-                        setSummary(summary);
+                        console.log(response)
+                        setSummary(response.data);
                     })
                     .catch(error => console.error(error));
             })
@@ -34,28 +38,47 @@ export const Summary = () => {
 
     return (
         <div className='Summary-view'>
-            <h1>Words</h1>
-            <form onSubmit={handleFormSubmit}>
-                <input type="radio" name="words" id="50" value="50" onChange={handleWordCountChange} />
-                <label htmlFor="50">50</label>
-                <br />
-                <input type="radio" name="words" id="100" value="100" onChange={handleWordCountChange} defaultChecked />
-                <label htmlFor="100">100</label>
-                <br />
-                <input type="radio" name="words" id="150" value="150" onChange={handleWordCountChange} />
-                <label htmlFor="150">150</label>
-                <br />
-                <input type="radio" name="words" id="200" value="200" onChange={handleWordCountChange} />
-                <label htmlFor="200">200</label>
-                <br />
-                <button type="submit">Summarize</button>
-            </form>
-            <p>GPT Model: {typeof summary === 'string' ? summary : JSON.stringify(summary.summary.model)}</p>
-            <p>Total Tokens: {typeof summary === 'string' ? summary : JSON.stringify(summary.summary.usage.total_tokens)}</p>
-            <p>Summary: {typeof summary === 'string' ? summary : JSON.stringify(summary.summary.choices[0].message.content)}</p>
-            <p>Created at: {typeof summary === 'string' ? summary : JSON.stringify(summary.summary.created)}</p>
-            <p>Created at: {typeof summary === 'string' ? summary : new Date(summary.summary.created * 1000).toLocaleString()} UTC</p>
-            <p>Complete API Response: {typeof summary === 'string' ? summary : JSON.stringify(summary)}</p>
+            <h1>Summarize</h1>
+            {
+                summary !== "" ?
+                    summary.hasOwnProperty("error") ?
+                        <div className="error-fields">
+                            <p>Status: Error</p>
+                            <p>Error Message: {summary.error}</p>
+                        </div>
+                        :
+                        <div className="idk">
+                            <div className="summary-fields">
+                                <p>Status: Successful</p>
+                                <p>GPT Model: {typeof summary === 'string' ? summary : summary.model}</p>
+                                <p>Total Tokens: {typeof summary === 'string' ? summary : JSON.stringify(summary.usage.total_tokens)}</p>
+                                <p>Summary: {typeof summary === 'string' ? summary : JSON.stringify(summary.choices[0].message.content)}</p>
+                                <p>Created at: {typeof summary === 'string' ? summary : JSON.stringify(new Date(summary.created * 1000).toLocaleString())} UTC</p>
+                                <p>Complete API Response: {typeof summary === 'string' ? summary : JSON.stringify(summary)}</p>
+                            </div>
+                            <form onSubmit={clearSummary}>
+                                <button type="submit">New Summary</button>
+                            </form>
+                        </div>
+                    :
+                    <form onSubmit={handleFormSubmit}>
+                        <h2>Words</h2>
+                        <input type="radio" name="words" id="50" value="50" onChange={handleWordCountChange} />
+                        <label htmlFor="50">50</label>
+                        <br />
+                        <input type="radio" name="words" id="100" value="100" onChange={handleWordCountChange} defaultChecked />
+                        <label htmlFor="100">100</label>
+                        <br />
+                        <input type="radio" name="words" id="150" value="150" onChange={handleWordCountChange} />
+                        <label htmlFor="150">150</label>
+                        <br />
+                        <input type="radio" name="words" id="200" value="200" onChange={handleWordCountChange} />
+                        <label htmlFor="200">200</label>
+                        <br />
+                        <button type="submit">Summarize</button>
+                    </form>
+
+            }
         </div>
     );
 };

50938629d4d88a63b6f943b3f06fee9283e47408
luisKisters <luis.w.kisters@gmail.com>
2023-05-11 16:11:32 +0200

    Added view for viewing selected article

--- article/views.py
+++ article/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from .article_api import get_articles
 import json
+from django.core.cache import cache
 
 
 def article_view(request, topic='bitcoin', period=7):
@@ -22,3 +23,18 @@ def article_view(request, topic='bitcoin', period=7):
     """
     data = get_articles(topic=topic, period=period)
     return JsonResponse(data, safe=False)
+
+
+def current_article_view(request):
+    if request.method == 'POST':
+        article_data = request.body.decode('utf-8')
+        cache.set('article_data', article_data,
+                  timeout=3600)  # Cache for 1 hour
+        return JsonResponse({'message': 'Article data saved successfully'})
+    elif request.method == 'GET':
+        article_data = cache.get('article_data')
+        if article_data is not None:
+            article_data = json.loads(article_data)
+            return JsonResponse(article_data)
+        else:
+            return JsonResponse({'error': 'No article'})
